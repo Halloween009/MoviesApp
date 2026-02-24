813b0f8a44f207081241d3f6e72ace73c5e67804
Tasha <konb507@gmail.com>
2026-02-24 15:31:46 +0900

    big

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -11,13 +10,6 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
-  },
-  webpack(config) {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname),
-    };
-    return config;
   },
 };
 
