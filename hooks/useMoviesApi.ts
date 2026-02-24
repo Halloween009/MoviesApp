343d8eba9584b2025c745d19f6ef34a1f5ee1b1b
Tasha <konb507@gmail.com>
2026-02-24 15:22:43 +0900

    big

--- hooks/useMoviesApi.ts
+++ hooks/useMoviesApi.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Movie, Genre } from "@/types";
+import { Movie } from "@/types";
 import { useDebounce } from "./useDebounce";
 import { getCookie } from "@/app/util/getCookie";
 
